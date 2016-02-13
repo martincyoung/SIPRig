75b1d8805ab40b8a48e0ea72149bb8b2dbec4ac3
Martin Young <martin.young@cantab.net>
2016-02-13 01:03:00 -0800

    Add test stubs for Request class

---
+++ tests/test_request.py
@@ -0,0 +1,47 @@
+from nose.tools import assert_true
+
+from siprig import Request
+
+
+class TestRequest():
+    def test_get_req_from_file(self):
+        assert_true(False)
+
+    def test_validate_valid_input(self):
+        assert_true(False)
+
+    def test_validate_invalid_input(self):
+        assert_true(False)
+
+    def test_no_validate_valid_input(self):
+        assert_true(False)
+
+    def test_no_validate_invalid_input(self):
+        assert_true(False)
+
+    def test_no_validate_invalid_input_quiet(self):
+        assert_true(False)
+
+    def test_add_blank_lines_on_valid_input(self):
+        assert_true(False)
+
+    def test_add_blank_lines_to_one_blank_line_at_file_end(self):
+        assert_true(False)
+
+    def test_add_blank_lines_to_no_blank_lines_at_file_end(self):
+        assert_true(False)
+
+    def test_protocol_udp(self):
+        assert_true(False)
+
+    def test_protocol_tcp(self):
+        assert_true(False)
+
+    def test_protocol_no_protocol(self):
+        assert_true(False)
+
+    def test_protocol_unrecognized_protocol(self):
+        assert_true(False)
+
+    def test_protocol_multple_protocol(self):
+        assert_true(False)
